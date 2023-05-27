652eccf4f74ee7dc26c2adbbc4cd0915b8819785
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2023-05-27 19:06:51 -0300

    adicionando plugin de extração do css

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
-const path = require('path')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader', 'css-loader'
-                ]
-            }
-        ]
-    }
-}    
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css",
+    }),
+  ],
+};

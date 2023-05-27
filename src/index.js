2b909c011d4ca5824c7df4fc53fac75229d7c92d
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2023-05-27 18:51:42 -0300

    configurando loader de css básico

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import sum from './calc.js'
+import Heading from './components/heading.js';
 
 console.log('Webpack 5')
 
 sum(1, 2);
 sum (3, 5);
+
+// Loaders
+const heading = new Heading();
+
+heading.create("Este título é dinâmico")
